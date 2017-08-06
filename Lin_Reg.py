d0c176fff5197af56355a52aecd89903a3181e66
Peter Kokalov <petervkokalov@gmail.com>
2017-08-06 14:39:29 -0700

    Update Lin_Reg.py

--- Lin_Reg.py
+++ Lin_Reg.py
@@ -2,71 +2,59 @@ import pandas as pd
 import numpy as np
 
 def data():
-    data = pd.read_csv('ex1data1.txt')
+    data = pd.read_csv('C:\\Users\\PeterKokalov\\lpthw\\Machine_Learning\\Week1-2\\machine-learning-ex1\\ex1\\ex1data1.txt')
     data.columns = ['Profit','Population']
     ones = np.zeros((len(data)))
     for i in range(len(ones)):
         ones[i] = 1
-
+    theta = [0, 1.5]
     data['Ones'] = ones
     X = data
-    X = pd.DataFrame(X, columns=['Profit', 'Ones'])
+    X = pd.DataFrame(X, columns=['Ones', 'Profit'])
     y = data['Population']
-
+    
     return X, y
 
-def hypothesis(theta, X):
-    obs = theta[0] + theta[1] * X
-    return obs
-
-def costFunctionJ(X, y, theta):
-    SSE = 0
-    m = len(y)
-    X = X['Profit']
-    for i in range(m):
-        x = X.ix[i]
-        y_i = y.ix[i]
-        h_x_i = hypothesis(theta, x)
-        Error = (h_x_i - y_i)
-        SSE += Error
-    SSE = SSE**2
-    cost_out = SSE * (1/(2 * m))
-
-    return cost_out
-
-def costFunctionJ_mult(X, y, theta, x_i):
+def cost_func(X, y, theta, x_i):
     SSE = 0
     m = len(y)
     for i in range(m):
         x = X.ix[i] 
         y_i = y.ix[i] 
-        h_x_i = hypothesis(theta, x) 
-        Error = (h_x_i - y_i) * x[x_i]
-        SSE += Error
-    cost_out = SSE * (1/(2 * m))
+        h_x_i = theta[0] + theta[1] * x 
+        loss = (h_x_i - y_i) * x[x_i] 
+        SSE += loss
+    total_loss = SSE * (1/m)
 
-    return cost_out
+    return total_loss
 
 def gradientDescent(X, y, alpha, theta):
-    thetas = [] 
+    thetas = [] # holding all possible theta's ( in this case 2 )
     m = len(y)
-    constant = (alpha * (1/m))
     for x_i in range(len(theta)):
-        cost = costFunctionJ_mult(X, y, theta, x_i)
-        convergence_alpha = theta[x_i] - (constant * cost)
+        cost = cost_func(X, y, theta, x_i)
+        convergence_alpha = theta[x_i] - ((alpha * (1/m)) * cost)
         thetas.append(convergence_alpha)
 
     return thetas
 
 def linreg(X, y, alpha, theta, iterations):
+    descent_markers = []
     for i in range(iterations):
         optimal_theta = gradientDescent(X, y, alpha, theta)
+        descent_markers.append(optimal_theta)
         theta = optimal_theta
-        
+
     return theta
 
 def main():
     X, y = data()
     theta = [0,0]
     alpha = 0.01
-    iterations = 1000
+    iterations = 1500
+    opt_theta = linreg(X, y, alpha, theta, iterations)
+
+    return opt_theta
+
+if __name__ == '__main__':
+    main()
