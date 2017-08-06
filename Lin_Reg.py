b9ab17cccc71e22df06397e320d002625070ac7c
Peter Kokalov <petervkokalov@gmail.com>
2017-08-06 08:51:40 -0700

    Add files via upload

---
+++ Lin_Reg.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+
+def data():
+    data = pd.read_csv('ex1data1.txt')
+    data.columns = ['Profit','Population']
+    ones = np.zeros((len(data)))
+    for i in range(len(ones)):
+        ones[i] = 1
+
+    data['Ones'] = ones
+    X = data
+    X = pd.DataFrame(X, columns=['Profit', 'Ones'])
+    y = data['Population']
+
+    return X, y
+
+def hypothesis(theta, X):
+    obs = theta[0] + theta[1] * X
+    return obs
+
+def costFunctionJ(X, y, theta):
+    SSE = 0
+    m = len(y)
+    X = X['Profit']
+    for i in range(m):
+        x = X.ix[i]
+        y_i = y.ix[i]
+        h_x_i = hypothesis(theta, x)
+        Error = (h_x_i - y_i)
+        SSE += Error
+    SSE = SSE**2
+    cost_out = SSE / (2*m)
+
+    return cost_out
+
+def costFunctionJ_mult(X, y, theta, x_i):
+    SSE = 0
+    m = len(y)
+    for i in range(m):
+        x = X.ix[i]
+        y_i = y.ix[i]
+        h_x_i = hypothesis(theta, x)
+        Error = (h_x_i - y_i) * x[x_i]
+        SSE += Error
+    cost_out = SSE * (1/(2*m))
+
+    return cost_out
+
+def gradientDescent(X, y, alpha, theta):
+    thetas = [1,1.5]
+    m = len(y)
+    constant = (alpha * (1/m))
+    for x_i in range(len(theta)):
+        cost = costFunctionJ_mult(X, y, theta, x_i)
+        convergence_alpha = theta[x_i] - (constant * cost)
+        thetas[x_i] = convergence_alpha
+
+    return thetas
+
+def linreg():
+    X, y = data()
+    theta = [1,1.5]
+    alpha = 0.01
+    optimal_theta = gradientDescent(X, y, alpha, theta)
+    print("Linear Regression on dataset returned theta values of: {}".format(optimal_theta))
+
+linreg()
+
+
+
+
