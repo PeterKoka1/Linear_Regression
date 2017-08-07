2af34eb346ee2ef68846b4fdce28c2460fc4028c
Peter Kokalov <petervkokalov@gmail.com>
2017-08-07 07:50:20 -0700

    Update Lin_Reg.py

--- Lin_Reg.py
+++ Lin_Reg.py
@@ -2,63 +2,77 @@ import pandas as pd
 import numpy as np
 
 def data():
-    data = pd.read_csv('C:\\Users\\PeterKokalov\\lpthw\\Machine_Learning\\Week1-2\\machine-learning-ex1\\ex1\\ex1data1.txt')
+    data = pd.read_csv('ex1data1.txt')
     data.columns = ['Profit','Population']
     ones = np.zeros((len(data)))
     for i in range(len(ones)):
         ones[i] = 1
 
-    theta = np.zeros(shape=(2,1))
-    iterations = 1500
-    alpha = 0.01
-
     data['Ones'] = ones
     X = data
     X = pd.DataFrame(X, columns=['Ones', 'Profit'])
     y = data['Population']
-    
+
     return X, y
+
+def hypothesis(theta, X):
+    obs = theta[0] + theta[1] * X
+    return obs
+
+def costFunctionJ(X, y, theta):
+    SSE = 0
+    m = len(y)
+    X = X['Profit']
+    for i in range(m):
+        x = X.ix[i]
+        y_i = y.ix[i]
+        h_x_i = hypothesis(theta, x)
+        loss = (h_x_i - y_i)
+        SSE += loss
+    SSE = SSE**2
+    cost_out = SSE * (1/(2 * m))
+
+    return cost_out
 
 def cost_func(X, y, theta, x_i):
     SSE = 0
-    m = y.size
+    m = len(y)
     for i in range(m):
         x = X.ix[i] 
         y_i = y.ix[i] 
-        h_x_i = theta[0] + theta[1] * x 
-        loss = (h_x_i - y_i) * x[x_i] 
-        SSE += loss
-    total_loss = SSE * (1/m)
+        h_x_i = hypothesis(theta, x) 
+        Error = (h_x_i - y_i) * x[x_i] 
+        SSE += Error 
+    cost_out = SSE * (1/m)
 
-    return total_loss
+    return cost_out
 
 def gradientDescent(X, y, alpha, theta):
-    thetas = [] # holding all possible theta's ( in this case 2 )
-    m = y.size
+    thetas = [] 
+    m = len(y)
+    const = (alpha * (1/m))
     for x_i in range(len(theta)):
         cost = cost_func(X, y, theta, x_i)
-        convergence_alpha = theta[x_i] - ((alpha * (1/m)) * cost)
+        convergence_alpha = theta[x_i] - (const * cost)
         thetas.append(convergence_alpha)
 
     return thetas
 
 def linreg(X, y, alpha, theta, iterations):
-    descent_markers = []
     for i in range(iterations):
         optimal_theta = gradientDescent(X, y, alpha, theta)
-        descent_markers.append(optimal_theta)
         theta = optimal_theta
 
     return theta
 
 def main():
     X, y = data()
-    theta = np.zeros(size=(2,1))
+    theta = [0,0]
     alpha = 0.01
     iterations = 1500
     opt_theta = linreg(X, y, alpha, theta, iterations)
 
     return opt_theta
-
-if __name__ == '__main__':
-    main()
+#
+# if __name__ == '__main__':
+#     main()
